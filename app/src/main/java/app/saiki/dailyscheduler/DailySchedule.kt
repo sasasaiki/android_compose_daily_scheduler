44b7f89aa08620cc88b48898b34c479b7a4894a9
s-iijima <s-iijima@unext.jp>
2024-06-20 15:36:34 +0900

    add: Eventの配置OK

--- app/src/main/java/app/saiki/dailyscheduler/DailySchedule.kt
+++ app/src/main/java/app/saiki/dailyscheduler/DailySchedule.kt
@@ -1,8 +1,11 @@
 package app.saiki.dailyscheduler
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -13,16 +16,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.ParentDataModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import kotlin.math.roundToInt
 
 @Composable
 fun DailySchedule(
     modifier: Modifier = Modifier,
-    events: List<CalendarEvent>
+    targetDate: LocalDateTime = LocalDateTime.now(),
+    events: List<CalendarEvent>,
+    eventContent: @Composable (CalendarEvent) -> Unit = { event ->
+        EventItem(event = event)
+    }
 ) {
     val hourHeight = 60.dp
 
@@ -31,15 +42,30 @@ fun DailySchedule(
         Text(text = "hoge")
     }
 
-    val now = LocalDateTime.now()
     val sideBarTimeLabels = @Composable {
         repeat(24) { i ->
-            SidebarTimeLabel(LocalDateTime.of(now.year, now.month, now.dayOfMonth, i, 0))
+            val dateTime = LocalDateTime.of(
+                targetDate.year,
+                targetDate.month,
+                targetDate.dayOfMonth,
+                i,
+                0
+            )
+            Box(
+                modifier = Modifier.timeLabelDataModifier(
+                    dateTime
+                )
+            ) {
+                SidebarTimeLabel(dateTime)
+            }
         }
     }
     val calenderEvents = @Composable {
         events.sortedBy { it.startTime }.forEach { event ->
-            EventItem(event = event)
+            // EventItem固定でいいならBoxなしで直接つけても動く
+            Box(modifier = Modifier.calenderEventModifier(event)) {
+                eventContent(event)
+            }
         }
     }
 
@@ -51,40 +77,58 @@ fun DailySchedule(
         modifier = modifier
             .fillMaxHeight()
             .verticalScroll(state = rememberScrollState())
-            .background(MaterialTheme.colorScheme.secondary),
+            .background(MaterialTheme.colorScheme.surface),
     ) { (timeLabelMeasureables, eventMeasureables), constraints ->
-        val totalHeight = hourHeight.roundToPx() * 24
+        println("saiki 呼ばれてる？")
+        val totalHeight = hourHeight.roundToPx() * timeLabelMeasureables.size
+        var offsetY = 0
+        val yPositionMap = mutableMapOf<LocalDateTime, Int>()
+        var labelMaxWidth = 0
         val placablesTimeLabel = timeLabelMeasureables.map { measurable ->
-            measurable.measure(
+            yPositionMap[(measurable.parentData as LocalDateTime)] = offsetY
+            offsetY += hourHeight.roundToPx()
+
+            val placeable = measurable.measure(
                 constraints.copy(
                     minHeight = hourHeight.roundToPx(),
                     maxHeight = hourHeight.roundToPx()
                 )
             )
+
+            labelMaxWidth = maxOf(labelMaxWidth, placeable.width)
+
+            placeable
         }
-//
-//        val placeablesWithEvents = measureables.map { measurable ->
-//            val event = measurable.parentData as Event
-//            val eventDurationMinutes = ChronoUnit.MINUTES.between(event.start, event.end)
-//            val eventHeight = ((eventDurationMinutes / 60f) * hourHeight.toPx()).roundToInt()
-//            val placeable = measurable.measure(
-//                constraints.copy(
-//                    minHeight = eventHeight,
-//                    maxHeight = eventHeight
-//                )
-//            )
-//            Pair(placeable, event)
-//        }
+
+        val placeablesWithEvents = eventMeasureables.map { measurable ->
+            val event = measurable.parentData as CalendarEvent
+            val eventDurationMinutes = ChronoUnit.MINUTES.between(event.startTime, event.endTime)
+            val eventHeight = ((eventDurationMinutes / 60f) * hourHeight.toPx()).roundToInt()
+            println("saiki ====================================")
+            println("saiki eventStartTime: ${event.startTime}")
+            println("saiki eventEndTime: ${event.endTime}")
+            println("saiki eventHeight: $eventHeight")
+            println("saiki =======================================")
+            val placeable = measurable.measure(
+                constraints.copy(
+                    maxWidth = constraints.maxWidth - labelMaxWidth,
+                    minHeight = eventHeight,
+                    maxHeight = eventHeight
+                )
+            )
+            println("saiki placeable.height: ${placeable.height}")
+            Pair(placeable, event)
+        }
         layout(constraints.maxWidth, totalHeight) {
             placablesTimeLabel.forEachIndexed { index, placeable ->
                 placeable.place(0, hourHeight.roundToPx() * index)
             }
-//            placeablesWithEvents.forEach { (placeable, event) ->
-//                val eventOffsetMinutes =
-//                    ChronoUnit.MINUTES.between(LocalTime.MIN, event.start.toLocalTime())
-//                val eventY = ((eventOffsetMinutes / 60f) * hourHeight.toPx()).roundToInt()
-//                placeable.place(0, eventY)
-//            }
+            placeablesWithEvents.forEach { (placeable, event) ->
+                val eventOffsetMinutes =
+                    ChronoUnit.MINUTES.between(LocalTime.MIN, event.startTime.toLocalTime())
+                val eventY = ((eventOffsetMinutes / 60f) * hourHeight.toPx()).roundToInt()
+                placeable.place(labelMaxWidth, eventY)
+            }
         }
     }
 }
@@ -96,14 +140,20 @@ fun EventItem(
     event: CalendarEvent
 ) {
     Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxSize(), // ここをMaxにしないとlayoutでどう測ってもwrapになってしまうので注意
         colors = CardDefaults
             .cardColors()
             .copy(
                 contentColor = MaterialTheme.colorScheme.onPrimary,
                 containerColor = MaterialTheme.colorScheme.primary
-            ),
+            )
     ) {
-        Column(modifier = modifier.padding(8.dp)) {
+        Column(
+            modifier = modifier
+                .padding(8.dp)
+        ) {
             Text(
                 text = "${event.startTime.format(EventTimeFormatter)} - ${
                     event.endTime.format(
@@ -126,7 +176,8 @@ fun SidebarTimeLabel(
     modifier: Modifier = Modifier,
 ) {
     Text(
-        modifier = Modifier.padding(4.dp),
+        modifier = Modifier
+            .padding(4.dp),
         text = time.format(HourLabelFormatter),
     )
 }
@@ -134,6 +185,25 @@ fun SidebarTimeLabel(
 private val EventTimeFormatter = DateTimeFormatter.ofPattern("d:HH:mm")
 private val HourLabelFormatter = DateTimeFormatter.ofPattern("HH:mm")
 
+// Parent Data の定義
+class TimeLabelDataModifier(val date: LocalDateTime) : ParentDataModifier {
+    override fun Density.modifyParentData(parentData: Any?): Any {
+        return date
+    }
+}
+
+// Parent Data Modifier の作成
+fun Modifier.timeLabelDataModifier(date: LocalDateTime) = this.then(
+    TimeLabelDataModifier(date)
+)
+
+
+// Parent Data Modifier の作成
+fun Modifier.calenderEventModifier(event: CalendarEvent) = this.then(
+    object : ParentDataModifier {
+        override fun Density.modifyParentData(parentData: Any?): Any = event
+    }
+)
 
 data class CalendarEvent(
     val title: String,
--- app/src/main/java/app/saiki/dailyscheduler/MainActivity.kt
+++ app/src/main/java/app/saiki/dailyscheduler/MainActivity.kt
@@ -12,19 +12,97 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.saiki.dailyscheduler.ui.theme.DailySchedulerComposeTheme
+import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val targetDateTime = LocalDateTime.now()
         setContent {
             DailySchedulerComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    DailySchedule(events = emptyList(), modifier = Modifier.padding(innerPadding))
+                    DailySchedule(
+                        targetDate = targetDateTime,
+                        events = createDummyEvent(targetDateTime),
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
+}
+
+fun createDummyEvent(targetDateTime: LocalDateTime): List<CalendarEvent> {
+    return listOf(
+        CalendarEvent(
+            startTime = LocalDateTime.of(
+                targetDateTime.year,
+                targetDateTime.month,
+                targetDateTime.dayOfMonth,
+                10,
+                0
+            ),
+            endTime = LocalDateTime.of(
+                targetDateTime.year,
+                targetDateTime.month,
+                targetDateTime.dayOfMonth,
+                11,
+                0
+            ),
+            title = "Meeting",
+        ),
+        CalendarEvent(
+            startTime = LocalDateTime.of(
+                targetDateTime.year,
+                targetDateTime.month,
+                targetDateTime.dayOfMonth,
+                11,
+                0
+            ),
+            endTime = LocalDateTime.of(
+                targetDateTime.year,
+                targetDateTime.month,
+                targetDateTime.dayOfMonth,
+                13,
+                0
+            ),
+            title = "Meeting",
+        ), CalendarEvent(
+            startTime = LocalDateTime.of(
+                targetDateTime.year,
+                targetDateTime.month,
+                targetDateTime.dayOfMonth,
+                13,
+                0
+            ),
+            endTime = LocalDateTime.of(
+                targetDateTime.year,
+                targetDateTime.month,
+                targetDateTime.dayOfMonth,
+                14,
+                0
+            ),
+            title = "Meeting",
+        ),
+        CalendarEvent(
+            startTime = LocalDateTime.of(
+                targetDateTime.year,
+                targetDateTime.month,
+                targetDateTime.dayOfMonth,
+                15,
+                0
+            ),
+            endTime = LocalDateTime.of(
+                targetDateTime.year,
+                targetDateTime.month,
+                targetDateTime.dayOfMonth,
+                17,
+                15
+            ),
+            title = "Meeting",
+        )
+    )
 }
 
 @Composable
